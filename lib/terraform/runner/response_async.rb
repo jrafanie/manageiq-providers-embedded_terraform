360d8930753f5987705e9b84af33974be47fc1d4
Manoj Puthran <putmanoj@in.ibm.com>
2024-03-22 21:09:39 -0700

    add method to stop/cancel async running terraform-runner stack job

--- lib/terraform/runner/response_async.rb
+++ lib/terraform/runner/response_async.rb
@@ -3,25 +3,43 @@ module Terraform
     class ResponseAsync
       include Vmdb::Logging
 
-      attr_reader :response, :debug
-
-      # Response object designed for holding full response from terraform-runner
+      # Response object designed for holding full response from terraform-runner stack job
       #
-      # @param response [Object] Terraform::Runner::Response object
-      # @param debug [Boolean] whether or not to delete base_dir after run (for debugging)
-      def initialize(response, debug: false)
-        @response = response
-        @debug    = debug
+      # @param stack_id [String] terraform-runner stack job - stack_id
+      def initialize(stack_id)
+        @stack_id = stack_id
       end
 
-      # @return [Boolean] true if the terraform job is still running, false when it's finished
+      # @return [Boolean] true if the terraform stack job is still running, false when it's finished
       def running?
-        response.status == "IN_PROGRESS"
+        return (@response.status == "" || @response.status == "IN_PROGRESS") if @response
+
+        false
       end
 
       # Stops the running Terraform job
       def stop
-        raise NotImplementedError, "Not yet impleted"
+        raise Error, "Not yet running" if !running?
+
+        Terraform::Runner.stop_async(@response.stack_id)
+      end
+
+      # Re-Fetch async job's response
+      def refresh_response
+        @response = Terraform::Runner.fetch_result_by_stack_id(@stack_id)
+
+        @response
+      end
+
+      # @return [Terraform::Runner::Response, NilClass] Response object with all details about the Terraform run, or nil
+      #         if the Terraform is still running
+      def response
+        # return if running?
+        return @response if @response
+
+        @response = Terraform::Runner.fetch_result_by_stack_id(@stack_id)
+
+        @response
       end
 
       # Dumps the Terraform::Runner::ResponseAsync into the hash
@@ -29,15 +47,15 @@ module Terraform
       # @return [Hash] Dumped Terraform::Runner::ResponseAsync object
       def dump
         {
-          :response => response,
-          :debug    => debug
+          :stack_id => @stack_id
         }
       end
 
       # Creates the Terraform::Runner::ResponseAsync object from hash data
       #
-      # @param hash [Hash] Dumped Ansible::Runner::ResponseAsync object
-      # @return [Terraform::Runner::ResponseAsync] Ansible::Runner::ResponseAsync Object created from hash data
+      # @param hash [Hash] Dumped Terraform::Runner::ResponseAsync object
+      #
+      # @return [Terraform::Runner::ResponseAsync] Terraform::Runner::ResponseAsync Object created from hash data
       def self.load(hash)
         # Dump dumps a hash and load accepts a hash, so we must expand the hash to kwargs as new expects kwargs
         new(**hash)
